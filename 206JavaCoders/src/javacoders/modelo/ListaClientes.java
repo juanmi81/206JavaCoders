50d5d13b5377950fac787a9ad967b923379f9275
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-20 19:31:22 +0100

    Update ListaClientes.java

--- 206JavaCoders/src/javacoders/modelo/ListaClientes.java
+++ 206JavaCoders/src/javacoders/modelo/ListaClientes.java
@@ -2,4 +2,11 @@ package javacoders.modelo;
 
 public class ListaClientes extends Lista<Cliente> {
 
+  // Ingresar nuevo cliente a la lista
+  ublic void addCliente(Cliente cliente) {
+    if(!lista.contains(cliente)) {
+			lista.add(cliente);
+		}
+	}
+  
 }
