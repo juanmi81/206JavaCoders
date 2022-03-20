1762d4dd92403f148f9665f6b9b77ce2611eac4e
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-20 19:02:44 +0100

    Update VMenuPrincipal.java

--- 206JavaCoders/src/javacoders/vista/VMenuPrincipal.java
+++ 206JavaCoders/src/javacoders/vista/VMenuPrincipal.java
@@ -7,10 +7,21 @@ public class VMenuPrincipal {
 	}
 	
 	public int getOpcion() {
-		int r = 2;
-			
-		
-		return r;
+		int op = 0;
+		try {
+			System.out.println(" 1. Gestión de Clientes \n 2. Gestión de Artículos \n 3. Gestión de Pedidos \n 4. Salir del programa");
+			op = sc.nextInt();
+			// si la opcion no esta entre 1 y 3 se ejecuta una excepciÃ³n
+			if ((op<1)||(op>4)) {
+				throw new Exception ("#La opciÃ³n es invalida");
+			}
+		} catch (Exception e) {
+			// en caso de excepciÃ³n se muestra el mensaje
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		// devuelve la opcion
+		return op;
 	}
 
 	
