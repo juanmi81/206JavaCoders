f76a4f61af740f7c342dd4a82d96eca5dca614ff
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-20 19:30:01 +0100

    Update Datos.java

--- 206JavaCoders/src/javacoders/modelo/Datos.java
+++ 206JavaCoders/src/javacoders/modelo/Datos.java
@@ -11,11 +11,24 @@ public class Datos {
 		listaClientes = new ListaClientes();
 		listaPedido = new ListaPedido();
 	}
-	
+	// ********* metodos y funciones de la Lista Articulos **************
+	// añadir un Articulo
 	public void addArticulo(Articulo articulo) {
 		listaArticulos.addArticulo(articulo);
 	}
+	
+	
+	// ********* metodos y funciones de la Lista Clientes **************
+	// añadir un cliente
+	public void addCliente(Cliente cliente) {
+		// llamar al metodo correspondiente pasando los datos del cliente
+		listaClientes.addCliente(Cliente cliente)
+	}
 
+	// ********* metodos y funciones de la Lista Pedidos **************
+	
+	
+	// ********* otros metodos y funciones ****************************
 	@Override
 	public String toString() {
 		return "Datos [listaArticulos=" + listaArticulos + ", listaClientes=" + listaClientes + ", listaPedido="
