8f1cd16cf3205ec0e910208bda464bf07dfbe86b
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-21 23:55:57 +0100

    Update ListaPedido.java

--- 206JavaCoders/src/javacoders/modelo/ListaPedido.java
+++ 206JavaCoders/src/javacoders/modelo/ListaPedido.java
@@ -1,5 +1,10 @@
 package javacoders.modelo;
 
 public class ListaPedido extends Lista<Pedido> {
-
+  // agregar pedido
+  public void addPedido(Pedido a) {
+		if(!lista.contains(a)) {
+			lista.add(a);
+		}
+	}
 }
