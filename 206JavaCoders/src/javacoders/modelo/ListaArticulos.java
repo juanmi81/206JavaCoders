4485a4201d16c405a434cd99d5c536de45234a3b
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-21 23:53:34 +0100

    Update ListaArticulos.java

--- 206JavaCoders/src/javacoders/modelo/ListaArticulos.java
+++ 206JavaCoders/src/javacoders/modelo/ListaArticulos.java
@@ -1,13 +1,21 @@
 package javacoders.modelo;
 
 public class ListaArticulos extends Lista<Articulo> {
-
+	// añadir un articulo
 	public void addArticulo(Articulo a) {
 		if(!lista.contains(a)) {
 			lista.add(a);
 		}
 	}
-
+	// busca un articulo y lo devuelve
+	public Articulo existe(Articulo a) {
+		int indice = lista.indexOf(a);
+		if (indice !=-1) {
+			return lista.get(indice);
+		}
+		else return null;
+	}
+	
 	@Override
 	public String toString() {
 		return "ListaArticulos [lista=" + lista + "]";
