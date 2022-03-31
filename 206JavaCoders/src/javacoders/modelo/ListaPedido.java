a820906bf5345cf6a5cdbee4b1900f4bbfdfb180
juanmi81 <101145721+juanmi81@users.noreply.github.com>
2022-03-31 15:20:11 +0200

    Update ListaPedido.java

--- 206JavaCoders/src/javacoders/modelo/ListaPedido.java
+++ 206JavaCoders/src/javacoders/modelo/ListaPedido.java
@@ -20,4 +20,57 @@ public class ListaPedido extends Lista<Pedido> {
 		int indice = lista.indexOf(c);
 		lista.remove(indice);
 	}
+	// mostrar pendientes
+	public String pendiente() {
+		String pendent= "";
+		for (Pedido tmp: this.lista) {
+			// si el pedido esta pendiente
+			if () {
+				pendent += tmp.toString();
+			} 
+		}
+		return pendent;
+	}
+	// mostrar pendientes filtrado por cliente
+	public String pendiente(String codcli) {
+		String pendent= "";
+		for (Pedido tmp: this.lista) {
+			// si el pedido es del cliente
+			if () {
+				// si el pedido esta pendiente
+				if() {	
+				pendent += tmp.toString();
+				}
+			} 
+		}
+		return pendent;
+	}
+	// mostrar enviados
+	public String enviados() {
+		String envio= "";
+		for (Pedido tmp: this.lista) {
+			// si el pedido es del cliente
+			if () {
+				// si el pedido esta pendiente
+				if() {	
+				envio += tmp.toString();
+				}
+			} 
+		}
+		return envio;
+	}
+	// mostrar enviados filtrado por cliente
+	public String enviados(String codcli) {
+		String envio= "";
+		for (Pedido tmp: this.lista) {
+			// si el pedido es del cliente
+			if () {
+				// si el pedido esta pendiente
+				if() {	
+				envio += tmp.toString();
+				}
+			} 
+		}
+		return envio;
+	}
 }
